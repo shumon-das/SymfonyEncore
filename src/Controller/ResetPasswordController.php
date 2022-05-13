74ac9dd8d9da1dd18a6225458ba94b85555c68d2
monoranjan.das <monoranjan.das@covianalytics.com>
2022-05-13 12:02:13 +0600

    Added beantalkd messenger for send mail by queue

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\ChangePasswordFormType;
 use App\Form\ResetPasswordRequestFormType;
+use App\Message\PracticeMail;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use RuntimeException;
@@ -14,6 +15,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -39,7 +41,7 @@ class ResetPasswordController extends AbstractController
      * Display & process form to request a password reset.
      */
     #[Route('/forgot-password', name: 'app_forgot_password_request')]
-    public function request(Request $request, MailerInterface $mailer): Response
+    public function request(Request $request, MessageBusInterface $messageBus): Response
     {
         $form = $this->createForm(ResetPasswordRequestFormType::class);
         $form->handleRequest($request);
@@ -47,7 +49,7 @@ class ResetPasswordController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             return $this->processSendingPasswordResetEmail(
                 $form->get('email')->getData(),
-                $mailer
+                $messageBus
             );
         }
 
@@ -130,66 +132,7 @@ class ResetPasswordController extends AbstractController
         ]);
     }
 
-//    /**
-//     * Validates and process the reset URL that the user clicked in their email.
-//     */
-//    #[Route('/reset/{token}', name: 'app_reset_password')]
-//    public function reset(Request $request, UserPasswordHasherInterface $userPasswordHasher, string $token = null): Response
-//    {
-//        if ($token) {
-//            // We store the token in session and remove it from the URL, to avoid the URL being
-//            // loaded in a browser and potentially leaking the token to 3rd party JavaScript.
-//            $this->storeTokenInSession($token);
-//
-//            return $this->redirectToRoute('app_reset_password');
-//        }
-//
-//        $token = $this->getTokenFromSession();
-//        if (null === $token) {
-//            throw $this->createNotFoundException('No reset password token found in the URL or in the session.');
-//        }
-//
-//        try {
-//            $user = $this->resetPasswordHelper->validateTokenAndFetchUser($token);
-//        } catch (ResetPasswordExceptionInterface $e) {
-//            $this->addFlash('reset_password_error', sprintf(
-//                '%s - %s',
-//                ResetPasswordExceptionInterface::MESSAGE_PROBLEM_VALIDATE,
-//                $e->getReason()
-//            ));
-//
-//            return $this->redirectToRoute('app_forgot_password_request');
-//        }
-//
-//        // The token is valid; allow the user to change their password.
-//        $form = $this->createForm(ChangePasswordFormType::class);
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            // A password reset token should be used only once, remove it.
-//            $this->resetPasswordHelper->removeResetRequest($token);
-//
-//            // Encode(hash) the plain password, and set it.
-//            $encodedPassword = $userPasswordHasher->hashPassword(
-//                $user,
-//                $form->get('plainPassword')->getData()
-//            );
-//
-//            $user->setPassword($encodedPassword);
-//            $this->entityManager->flush();
-//
-//            // The session is cleaned up after the password has been changed.
-//            $this->cleanSessionAfterReset();
-//
-//            return $this->redirectToRoute('app_home');
-//        }
-//
-//        return $this->render('reset_password/reset.html.twig', [
-//            'resetForm' => $form->createView(),
-//        ]);
-//    }
-
-    private function processSendingPasswordResetEmail(string $emailFormData, MailerInterface $mailer): RedirectResponse
+    private function processSendingPasswordResetEmail(string $emailFormData, MessageBusInterface $messageBus): RedirectResponse
     {
         $user = $this->entityManager->getRepository(User::class)->findOneBy([
             'email' => $emailFormData,
@@ -216,17 +159,25 @@ class ResetPasswordController extends AbstractController
             return $this->redirectToRoute('app_check_email');
         }
 
-        $email = (new TemplatedEmail())
-            ->from(new Address('shumonsb@gmail.com', 'CoVi Analytics'))
-            ->to($user->getEmail())
-            ->subject('Your password reset request')
-            ->htmlTemplate('reset_password/email.html.twig')
-            ->context([
-                'resetToken' => $resetToken,
-            ])
-        ;
+//        $email = (new TemplatedEmail())
+//            ->from(new Address('shumonsb@gmail.com', 'CoVi Analytics'))
+//            ->to($user->getEmail())
+//            ->subject('Your password reset request')
+//            ->htmlTemplate('reset_password/email.html.twig')
+//            ->context([
+//                'resetToken' => $resetToken,
+//            ])
+//        ;
+        $data = [
+          'from' => 'shumonsb@gmail.com',
+          'fromName' => 'CoVi Analytics',
+          'to' => $user->getEmail(),
+          'subject' => 'Password reset request',
+          'template' => 'reset_password/email.html.twig',
+          'context' => $resetToken
+        ];
 
-        $mailer->send($email);
+        $messageBus->dispatch(new PracticeMail($data));
 
         // Store the token object in session for retrieval in check-email route.
         $this->setTokenObjectInSession($resetToken);
