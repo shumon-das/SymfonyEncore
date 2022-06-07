4233c983116f0304690eb8938d2a41bb70c4c9ce
monoranjan.das <monoranjan.das@covianalytics.com>
2022-06-08 00:24:17 +0600

    complete dev.log

--- src/Controller/StoryController.php
+++ src/Controller/StoryController.php
@@ -2,16 +2,65 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class StoryController extends AbstractController
+#[Route('/story/write', name: 'write_story')]
+class StoryController extends AbstractApiController
 {
-    #[Route('/story', name: 'app_story')]
-    public function index(): Response
+//    #[Route('/story/write', name: 'write_story')]
+//    public function writeStory(): Response
+//    {
+//        return $this->render('story/write.html.twig', [
+//            'controller_name' => 'StoryController',
+//        ]);
+//    }
+//
+//    #[Route('/stories', name: 'get_stories')]
+//    public function getStories(): Response
+//    {
+//        return $this->render('story/write.html.twig', [
+//            'controller_name' => 'StoryController',
+//        ]);
+//    }
+//
+//    #[Route('/story/{id}}', name: 'get_story')]
+//    public function getStory(): Response
+//    {
+//        return $this->render('story/write.html.twig', [
+//            'controller_name' => 'StoryController',
+//        ]);
+//    }
+//
+//    #[Route('/story/{id}', name: 'update_story')]
+//    public function updateStory(): Response
+//    {
+//        return $this->render('story/write.html.twig', [
+//            'controller_name' => 'StoryController',
+//        ]);
+//    }
+//
+//    #[Route('/story/{id}', name: 'delete_story')]
+//    public function deleteStory(): Response
+//    {
+//        return $this->render('story/write.html.twig', [
+//            'controller_name' => 'StoryController',
+//        ]);
+//    }
+//
+//    #[Route('/story/{idc}', name: 'stories_by_story')]
+//    public function getStoriesByIdc(): Response
+//    {
+//        return $this->render('story/write.html.twig', [
+//            'controller_name' => 'StoryController',
+//        ]);
+//    }
+
+
+    public function action(): Response
     {
-        return $this->render('story/index.html.twig', [
+        return $this->render('story/write.html.twig', [
             'controller_name' => 'StoryController',
         ]);
     }
